53dcf19d552d2b9da94e213904b9c04feb2ab8e8
Ben Sladewski <bsladewski@gmail.com>
2023-12-19 19:05:31 -0500

    Reworked coin pickups.

--- Assets/Scripts/Pickups/Pickup.cs
+++ Assets/Scripts/Pickups/Pickup.cs
@@ -5,29 +5,15 @@ public class Pickup : MonoBehaviour
 {
     [Required]
     [SerializeField]
-    private PickupSO pickupSO;
-
-    [Required]
-    [SerializeField]
-    private Transform pickupVisualSpawn;
-
-    private void Awake()
-    {
-        Instantiate(
-            pickupSO.GetPickupVisualPrefab(),
-            pickupVisualSpawn.position,
-            pickupSO.GetPickupVisualPrefab().transform.rotation,
-            transform
-        );
-    }
+    private PickupVisual pickupVisual;
 
     public void GetPickup()
     {
-        if (pickupSO.GetPickupParticleSystemPrefab() != null)
+        if (pickupVisual.GetPickupParticleSystemPrefab() != null)
         {
             Instantiate(
-                pickupSO.GetPickupParticleSystemPrefab(),
-                pickupVisualSpawn.position,
+                pickupVisual.GetPickupParticleSystemPrefab(),
+                pickupVisual.transform.position,
                 Quaternion.identity
             );
         }
