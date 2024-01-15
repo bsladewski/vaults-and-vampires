8ef262f8f62028d45711f85e03340ba2e1cc1d0c
Ben Sladewski <bsladewski@gmail.com>
2024-01-14 19:08:29 -0500

    Initial implementation of player health UI.

--- Assets/Scripts/Utils/HealthManager.cs
+++ Assets/Scripts/Utils/HealthManager.cs
@@ -5,6 +5,10 @@ namespace Utils
 {
     public class HealthManager : MonoBehaviour
     {
+        public Action OnHealthGained;
+
+        public Action OnHealthLost;
+
         public Action OnDeath;
 
         [SerializeField]
@@ -20,15 +24,36 @@ namespace Utils
         public void UpdateHealth(int amount)
         {
             currentHealth = Math.Clamp(currentHealth + amount, 0, maxHealth);
+
+            if (amount > 0)
+            {
+                OnHealthGained?.Invoke();
+            }
+            else if (amount < 0)
+            {
+                OnHealthLost?.Invoke();
+            }
+
             if (currentHealth == 0)
             {
                 OnDeath?.Invoke();
             }
         }
 
+        public int GetMaxHealth()
+        {
+            return maxHealth;
+        }
+
+        public int GetCurrentHealth()
+        {
+            return currentHealth;
+        }
+
         public void ResetHealth()
         {
             currentHealth = maxHealth;
+            OnHealthGained?.Invoke();
         }
     }
 }
